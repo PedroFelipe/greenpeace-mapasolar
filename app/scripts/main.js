f537b0a1852faa732f3eeca5b4439058f85e8282
Pedro Felipe <pedro@pedrofelipe.com.br>
2015-02-10 23:51:24 -0300

    estrutura refeita; menos tags

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,21 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+/*var bHeight = 0;
+
+$('#content .container *').each(function(){
+ if($(this).height() > bHeight) {
+   bHeight = $(this).height();
+ }
+});
+
+$('#content .container').height(bHeight);
+
+var bHeight2 = 0;
+
+$('#content .container *, #diy .container *').each(function(){
+ if($(this).height() > bHeight2) {
+   bHeight2 = $(this).height();
+ }
+});
+
+$('#diy .container').height(bHeight2);*/
